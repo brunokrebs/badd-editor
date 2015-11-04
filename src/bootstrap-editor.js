360de29fcdc0786410c21de7ee294a45f8189beb
Bruno Krebs <me@brunokrebs.com>
2015-11-03 23:32:12 -0200

    starting to build bootstrap-editor

--- src/bootstrap-editor.js
+++ src/bootstrap-editor.js
@@ -8,13 +8,24 @@
 (function() {
 	var editorModule = angular.module('bootstrapEditor', []);
 
+	var editorController = function($scope) {
+		// if attr are not set, use default values
+		$scope.title = angular.isDefined($scope.title) ? $scope.title : 'Bootstrap visual editor';
+		$scope.componentsTitle = angular.isDefined($scope.componentsTitle) ? $scope.componentsTitle : 'Components';
+		$scope.propertiesTitle = angular.isDefined($scope.propertiesTitle) ? $scope.propertiesTitle : 'Properties';
+	};
+	editorController.$inject = ['$scope'];
+
 	var editorDirective = function () {
 		return {
 			restrict: 'E',
 			templateUrl: 'bootstrap-editor.html',
 			scope: {
-				title: '@'
-			}
+				title: '@',
+				componentsTitle: '@',
+				propertiesTitle: '@'
+			},
+			controller: editorController
 		};
 	};
 
