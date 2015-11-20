4c476eebdce7b1051aae05c75ea599990317dc54
Bruno Krebs <me@brunokrebs.com>
2015-11-20 18:06:01 -0200

    range selection not working

--- src/badd-editor.js
+++ src/badd-editor.js
@@ -433,13 +433,13 @@
 
 			target.style.opacity = '0';
 
-			//var range = service.editableFrameDocument.createRange();
-			//range.setStart(service.editableFrameBody.firstElementChild, 0);
-			//range.setEnd(service.editableFrameBody.firstElementChild, 0);
-			//
-			//var selection = service.editableFrameDocument.getSelection();
-			//selection.removeAllRanges();
-			//selection.addRange(range);
+			var range = service.editableFrameDocument.createRange();
+			range.setStart(service.editableFrameBody.firstElementChild, 0);
+			range.setEnd(service.editableFrameBody.firstElementChild, 0);
+
+			var selection = service.editableFrameDocument.getSelection();
+			selection.removeAllRanges();
+			selection.addRange(range);
 		}
 
 		function getComputedCSSText(target) {
