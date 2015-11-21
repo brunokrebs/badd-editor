9c0485eb17f095114c0c379ce61b5e15d9185514
Bruno Krebs <me@brunokrebs.com>
2015-11-21 10:57:43 -0200

    focus working on FF, IE and Chrome

--- src/badd-editor.js
+++ src/badd-editor.js
@@ -435,7 +435,11 @@
 			target.style.opacity = '0';
 
 			service.editableFrame.contentWindow.focus();
+			service.editableFrameBody.firstElementChild.focus();
+
 			var selection = service.editableFrame.contentWindow.getSelection();
+			service.editableFrame.contentWindow.focus();
+			service.editableFrameBody.firstElementChild.focus();
 			selection.collapse(service.editableFrameBody.firstElementChild, 0);
 		}
 
