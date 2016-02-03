27af60818f29c85afc0e1ce246e09296a796c8db
Bruno Krebs <me@brunokrebs.com>
2016-02-03 21:45:28 -0200

    checking we must surround it or release it

--- src/js/badd-editor.js
+++ src/js/badd-editor.js
@@ -31,6 +31,21 @@
 			icon: 'fa fa-bold',
 			command: function(elements, doc) {
 				var i, element, range, strong, startOffset, endOffset;
+				var surround = false;
+
+				// we only surround the content if there are non strong elements selected
+				for (i = 0; i < elements.length; i++) {
+					element = elements[i];
+					if (element.node.nodeType == 1) {
+						continue;
+					}
+					surround = element.node.parentNode.tagName != 'STRONG';
+					if (surround) {
+						break;
+					}
+				}
+
+				// now we can process each selected element to surround/release it
 				for (i = 0; i < elements.length; i++) {
 					element = elements[i];
 					strong = doc.createElement('strong');
